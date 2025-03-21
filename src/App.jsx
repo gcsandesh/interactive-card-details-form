2d68f4a56a215b21bb8fe599780dc8873af6574a
gcsandesh <gcsandesh01@gmail.com>
2025-03-21 09:26:55 +0545

    feat: migrate project from CRA to vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Form from "./components/Form";
-import ContentAfterSubmit from "./components/ContentAfterSubmit.js";
+import ContentAfterSubmit from "./components/ContentAfterSubmit";
 function App() {
   let initialData = {
     cardHolderName: "Hari Bahadur",
